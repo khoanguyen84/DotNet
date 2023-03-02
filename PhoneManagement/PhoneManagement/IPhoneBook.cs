6f80087294d9cecef85fbd499c501a2444fc12d1
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-03-02 16:12:40 +0700

    Phone Management

---
+++ PhoneManagement/PhoneManagement/IPhoneBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneManagement
+{
+    internal interface IPhoneBook
+    {
+        bool InsertPhone(string name, string phone);
+        bool RemovePhone(string name);
+        bool UpdatePhone(string name, string newPhone);
+        void SearchPhone(string keyword);
+        void Sort();
+    }
+}
---
+++ StudentManage/AppDbContext/StudentDbContext.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using StudentManage.Entities;
+
+namespace StudentManage.AppDbContext
+{
+    internal class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Staff> Staffs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=khoanguyen\sqlexpress;Initial Catalog=StudentDb;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Staff>().HasData(
+                new Staff()
+                {
+                    StaffId = 1,
+                    Fullname = "Khoa Nguyễn",
+                    DateOfBirth = DateTime.Parse("10/10/2000")
+                },
+                new Staff()
+                {
+                    StaffId = 2,
+                    Fullname = "Quang Đặng",
+                    DateOfBirth = DateTime.Parse("09/09/2000")
+                }
+             );
+        }
+    }
+}
